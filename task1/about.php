f27ce0cb7bcd2a590813637d50d770e0e645f916
memmusty <memunati@gmail.com>
2017-12-07 10:56:12 +0100

    Add files via upload

---
+++ task1/about.php
@@ -0,0 +1,29 @@
+<html>
+<head>
+<title></title>
+<link type="text/css" rel="stylesheet" href="style.css">
+<style type="text/css">
+	p{
+	  	margin:0px 43% 0px 10%; 
+	  	text-align: justify;
+	  	font-size: 20px;
+	}
+	iframe{
+		position: absolute;
+		left:59%;
+		top: 45%;
+		width: 33%;
+		height: 36.5%;
+	}
+</style>
+</head>
+<body>
+	<?php include 'upper.html' ?>
+	<div id="lower"><br>
+	<p>Hi, I am <b>Memunat A. Ibrahim</b>, a Federal University Lokoja graduate of Computer Science. I love travelling, researching, anything adventurous in general; oh, I also love programming, photography, and repairing electronics (though, am not an engineer).I have interest in almost everything, I just love learning new things, and I can be very inquisitive. I am a critic,Ifind faults in things so easily that my friends call me an analyst (that analyses everything); I dont know if I should call that a gift or talent. Speaking of talents, I dont really know what my talents are, but I know I am multi-talented. I easily learn things, maybe its becauseIeasily understand things. <br>
+	Regarding programming, I have experience with visual basic, c, c++, java, html, css, javascript, php, mysql, lex, yacc, and micro-controllers programming. However, I work mostly with php, mysql, and java.<br> I have no professional experience yet, and hope to get much experienced soon. That's all I can think of for now, Bye!<br><b>Oh! before you go, enjoy the featured video at the rightward part of the page.</p>
+	<h2 style="position: absolute; left:59%; top: 37%;"> A Featured Video From Youtube</h2>
+	<iframe width="560" height="315" src="https://www.youtube.com/embed/7EWCTisKDSM" frameborder="0" gesture="media" allow="encrypted-media" allowfullscreen></iframe>
+	</div>
+</body>
+</html>
---
+++ task1/index.php
@@ -0,0 +1,38 @@
+<html>
+<head>
+<title></title>
+<link type="text/css" rel="stylesheet" href="style.css">
+</head>
+<body>
+	<?php include 'upper.html' ?>
+	<div id="lower">&nbsp;
+		<div id="skill">
+			<b style="font-size: 20px;">My Core Skills:</b>&nbsp; 
+			<span style="padding:5px; background-color: white;"> Web Development <img style="height: 15px; margin-top: 2px;" src="img/star.png"/><img style="height: 15px; margin-top: 2px;" src="img/star.png"/><img style="height: 15px; margin-top: 2px;" src="img/star.png"/>
+			</span> &nbsp; 
+			<span style="padding:5px; background-color: white;"> Database Management <img style="height: 15px; margin-top: 2px;" src="img/star.png"/><img 	style="height: 15px; margin-top: 2px;" src="img/star.png"/><img style="height: 15px; margin-top: 2px;" src="img/star.png"/><img style="height: 15px; margin-top: 2px;" src="img/star.png"/>
+			</span> &nbsp; 
+			<span style="padding:5px; background-color: white;"> Desktop Application Development <img style="height: 15px; margin-top: 2px;" src="img/star.png"/><img style="height: 15px; margin-top: 2px;" src="img/star.png"/><img style="height: 15px; margin-top: 2px;" src="img/star.png"/><img style="height: 15px; margin-top: 2px;" src="img/star.png"/>
+			</span>
+		</div>
+		<div id="bio">
+			<b style="font-size: 20px;">Professional Bio</b><br><br>
+			<span style="font-size: 17px;">Studied Computer Science at the Federal University Lokoja<br><br> 
+			I am currently enrolled in the sparkplug program, where to hope to gain some professional experience.</span>
+		</div>
+		<div id="names">
+			<b style="font-size: 20px;">Where I Write</b>
+			<table cellspacing="10px">
+				<tr>
+					<td><b>LinkedIn</b></td>
+					<td style="padding-left: 30px">Memmusty</td>
+				</tr>
+				<tr>
+					<td><b>Facebook</b></td>
+					<td style="padding-left: 30px">Memunat Juwon</td>
+				</tr>
+			</table>
+		</div>
+	</div>
+</body>
+</html>
